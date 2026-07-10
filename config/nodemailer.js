1f3224b08d79e2565f3a9a71890104b95fc85404
Aryvg <abenettilahun6@gmail.com>
2026-07-10 10:00:49 +0100

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,14 +1,16 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use STARTTLS
-    family: 4, // Force IPv4
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 module.exports = transporter;
