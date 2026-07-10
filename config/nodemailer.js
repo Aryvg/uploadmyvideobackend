1b5a15fc0e6d9ac9800ef2d5a95ce0efab69584b
Aryvg <abenettilahun6@gmail.com>
2026-07-10 10:06:51 +0100

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,16 +1,11 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 module.exports = transporter;
